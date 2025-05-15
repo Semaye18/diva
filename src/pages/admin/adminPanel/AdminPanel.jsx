3f0081932830357228df9124795561493bcc70c2
Səmayə Quliyeva <smayq28@gmail.com>
2025-05-15 13:43:44 +0400

    Add files via upload

--- src/pages/admin/adminPanel/AdminPanel.jsx
+++ src/pages/admin/adminPanel/AdminPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteProductThunk, getProductThunk, postProductThunk } from '../../../redux/reducers/productSlice';
 import { deleteBasketThunk, postBasketThunk } from '../../../redux/reducers/basketSlice';
@@ -6,12 +6,22 @@ import { useFormik } from 'formik';
 import AdminCard from '../../../components/common/cards/adminCard/AdminCard';
 
 const AdminPanel = () => {
+
+    const [text,setText]=useState('')
+    const [sort,setSort]=useState('asc')
+   
+   
+
+
+
+
+
     const dispatch=useDispatch()
     useEffect(()=>{
         dispatch(getProductThunk())
     },[])
     
-    const product=useSelector((state)=>state.products.products) || []
+    const products=useSelector((state)=>state.products.products) || []
     
     const deleteAdmin=(id=>{
         dispatch(deleteBasketThunk(id))
@@ -32,6 +42,11 @@ const AdminPanel = () => {
           dispatch(postProductThunk(values))
         },
       });
+      const filteredProducts=products
+      .filter(product => product.title.toLowerCase().includes(text.toLocaleLowerCase()))
+      .sort((a,b)=>{
+          return sort==="asc"? a.price-b.price: b.price-a.price
+      })
       return (
         <div>
         <form onSubmit={formik.handleSubmit}>
@@ -72,10 +87,12 @@ const AdminPanel = () => {
     </tr>
   </thead>
   <tbody>
-    {product && product.map(item => <AdminCard item={item}  deleteAdmin={()=>deleteAdmin(item._id)}/>)}
+    {filteredProducts && filteredProducts.map(item => <AdminCard item={item}  deleteAdmin={()=>deleteAdmin(item._id)}/>)}
   </tbody>
      </table>
-
+  <button onClick={()=>setSort("asc")}>coxdan aza</button>
+  <button onClick={()=>setSort("dasc")}>azdan coxa</button>
+  <input type="text" placeholder='ada gore axtaris' name='' id='' onChange={(e)=>setText(e.target.value)}/>
      </div>
       )
 }
